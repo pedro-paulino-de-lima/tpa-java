fd1f60118f8fb00bd9d006c27af27e5c43a80269
pedro-paulino-de-lima <164798663+pedro-paulino-de-lima@users.noreply.github.com>
2024-04-25 14:47:05 -0300

    Update gasto-combustivel.java

--- gasto-combustivel.java
+++ gasto-combustivel.java
@@ -1,1 +1,18 @@
+/******************************************************************************
 
+Código Gasto de Combustivel
+Aula de TPA
+
+*******************************************************************************/
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int distancia, gasto_medio;
+		Scanner input = new Scanner(System.in);
+		
+	    dista
+	}
+}
