125b5363618f9e76db7fe6c26c38c5b9fcd32f55
pedro-paulino-de-lima <164798663+pedro-paulino-de-lima@users.noreply.github.com>
2024-04-25 14:52:37 -0300

    Update gasto-combustivel.java

--- gasto-combustivel.java
+++ gasto-combustivel.java
@@ -10,9 +10,16 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-	    int distancia, gasto_medio;
+	    int distancia, gasto_medio, consumo_total;
 		Scanner input = new Scanner(System.in);
 		
-	    dista
+		System.out.println("Distancia");
+	    distancia = input.nextInt();
+	    System.out.println("Gasto Medio");
+	    gasto_medio = input.nextInt();
+	    
+	    consumo_total = distancia / gasto_medio;
+	    
+	    System.out.println("Consumo Total: " + consumo_total);
 	}
 }
